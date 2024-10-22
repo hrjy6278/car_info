a59e0152908a8c2467ac1c81f5c3e55cd41b4c78
hrjy6278 <hrjy6278@naver.com>
2024-10-22 18:49:19 +0900

    [style] 앱 컬러 구현

---
+++ lib/util/color/color.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 메인 컬러
+  static const Color primaryColor = Color(0xFF2C3E50); // 차콜 그레이
+  static const Color secondaryColor = Color(0xFFBDC3C7); // 쿨 그레이
+  static const Color accentColor = Color(0xFF1ABC9C); // 딥 민트
+  static const Color backgroundColor = Color(0xFFECF0F1); // 라이트 그레이
+}
+
+class AppTextColors {
+  // 어두운 배경에서 사용
+  static const Color primaryTextColor = Color(0xFFFFFFFF); // 화이트
+  static const Color secondaryTextColor = Color(0xFF7F8C8D); // 중간 회색
+  static const Color accentTextColor = Color(0xFF1ABC9C); // 딥 민트
+
+  // 밝은 배경에서 사용
+  static const Color darkTextColor = Color(0xFF2C3E50); // 차콜 그레이
+}
